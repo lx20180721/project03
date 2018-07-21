54f582409b9487359b7b6a62b3aa28b795f0fabe
Your Name <lx20160916@163.com>
2018-07-21 20:19:10 +0800

    添加drink和sleep方法

--- module01/src/com/demo/bean/Student.java
+++ module01/src/com/demo/bean/Student.java
@@ -8,4 +8,11 @@ public class Student extends Person {
     public void eat(){
         System.out.println("吃饭");
     }
+    public void drink(){
+        System.out.println("drink");
+    }
+    public void sleep(){
+        System.out.println("sleep");
+    }
+
 }
