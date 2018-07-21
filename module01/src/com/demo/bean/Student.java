0d184b782d7f3aaddeef337f9cbad5c25d9df44b
lx20180721 <41510306+lx20180721@users.noreply.github.com>
2018-07-21 20:21:58 +0800

    Update Student.java

--- module01/src/com/demo/bean/Student.java
+++ module01/src/com/demo/bean/Student.java
@@ -5,14 +5,17 @@ package com.demo.bean;
  * @date 2018年07月20日 18:25
  */
 public class Student extends Person {
+   //eat method
     public void eat(){
         System.out.println("吃饭");
     }
+    // drink method
     public void drink(){
         System.out.println("drink");
     }
+    // sleep method
     public void sleep(){
         System.out.println("sleep");
     }
-
+   
 }
